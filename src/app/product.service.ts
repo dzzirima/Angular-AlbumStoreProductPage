b756b39aabeafa59e7ee86b09b5f1a45a83c6a56
blackbox <dzzirima@gmail.com>
2021-12-03 12:55:10 +0200

    first commit on step3

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operator/map';
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { 
+
+  }
+
+  _albumUrl:string = "../assets/album.json"
+
+  getAlbum(id:number){
+     return this._http.get(this._albumUrl).map(response =>response.json())
+     
+
+  }
+}
