b260c7b0a926fae7ed2a6a7a72f431579d09ba84
blackbox <dzzirima@gmail.com>
2021-12-03 19:40:04 +0200

    phase 2 done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operator/map';
+
+import 'rxjs/add/operator/map'
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) { 
+  
 
-  }
+ private _albumUrl = '../assets/album.json'
 
- private _albumUrl:string = "../assets/album.json"
+ constructor(private _http: Http) { 
+
+}
 
   getAlbum(id:number){
      return this._http.get(this._albumUrl).map(response =>response.json())
-     
-
   }
 }
